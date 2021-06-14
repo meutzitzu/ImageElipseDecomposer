9b17647cd04168bfb5f0492f5bdd84774d2b7772
Lazăr Mihai <meutzitzu@gmail.com>
2021-06-14 07:59:16 +0000

    raster working

--- monteCarlo.py
+++ monteCarlo.py
@@ -30,16 +30,13 @@ def index():
   return render_template('index.html',generation=generation,svgString=Markup(fullSvgString),imgSize = IMG_SIZE);
 
 def random_ellipse(image):
-  center_coordinates = (random.randrange(image.shape[1] + 1),random.randrange(image.shape[0] + 1));
-  axesLength = (random.randrange(image.shape[0] + 1),random.randrange(image.shape[1] + 1));
-  angle = random.randrange(360);
-  startAngle = 0;
-  endAngle = 360;
+  start_coordinates = (random.randrange(image.shape[1] + 1),random.randrange(image.shape[0] + 1));
+  end_coordinates = (random.randrange(image.shape[1] + 1),random.randrange(image.shape[0] + 1));
   color = (random.randrange(256),random.randrange(256),random.randrange(256));
   thickness = -1;
   answer = image.copy();
-  answer = cv2.ellipse(answer,center_coordinates,axesLength,angle,startAngle,endAngle,color,thickness);
-  svgString = '<ellipse cx="%d" cy="%d" rx="%d" ry="%d" style="fill:rgb(%d,%d,%d)" transform="rotate(%d %d %d)"></ellipse>' % (*center_coordinates,*axesLength,color[2],color[1],color[0],angle,*center_coordinates);
+  answer = cv2.rectangle(answer,start_coordinates,end_coordinates ,color,thickness);
+  svgString = '<rect width="%d" height="%d" transform="translate(%d, %d)" style="fill:rgb(%d,%d,%d)"></rect>' % (start_coordinates[0]-end_coordinates[0],start_coordinates[1]-end_coordinates[1],*start_coordinates,color[2],color[1],color[0]);
   return (answer,svgString);
 
 def start_server():
